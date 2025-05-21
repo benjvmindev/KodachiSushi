8ea3ac273f535e549a1bc02c87d17a0a959b4713
benjaxd123 <ben.monsalve@duocuc.cl>
2025-05-21 01:59:45 -0400

    panel admin pendiente

--- app.py
+++ app.py
@@ -3,12 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def inicio():
+def index():
     return render_template('index.html')
 
 @app.route('/carrito')
 def carrito():
     return render_template('carrito.html')
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/ubicacion')
+def ubicacion():
+    return render_template('ubicacion.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
